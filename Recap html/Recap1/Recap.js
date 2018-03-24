a14cc37d1bad53e7775cdd6cb8ff713a675ab5bd
Tiina Vask <36072811+TiinaVask@users.noreply.github.com>
2018-03-24 02:21:01 -0700

    Lesson1

---
+++ Recap html/Recap1/Recap.js
@@ -0,0 +1,8 @@
+var para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  var name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+}
